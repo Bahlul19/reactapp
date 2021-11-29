7fbcd9be63c5daa1fa9d2cb56450964db224f8dc
Bahlul19 <bahlul.tausif@gmail.com>
2021-11-29 12:29:52 +0600

    done with states and props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import First from './component/First/First';
+// import Counter from './component/Counter/Counter';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+class App extends Component {
+
+  state = {
+    persons : [
+      {
+      name: 'SJ',
+      email: 'sjinnovation.com',
+      address: 'USA'
+      },
+
+      {
+        name: 'ABCD',
+        email: 'aaa.com',
+        address: 'BD'
+      }
+    ]
+  }
+
+  render(){
+    return(
+      <div className="App">
+
+      {/* <First name={ this.state.persons[0].name } email={ this.state.persons[0].email } address={ this.state.persons[0].address }/>
+      <First name='Bahlul' email='ab@gmail.com' address='Bd'/>
+      <First name='Siddiquee' email='siddiquee@gmail.com' address='SYL'/> */}
+
+      {/* <Counter /> */}
+
+      {
+        this.state.persons.map((people, index) => {
+          return <First 
+                        key={index}
+                        name={people.name}
+                        email={people.email} 
+                        address={people.address}
+                  />
+        })
+      }
+
     </div>
-  );
+    );
+}
 }
 
 export default App;
