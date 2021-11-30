6e2372505018cc80d504d64025d20ad4c68d0a44
Bahlul19 <bahlul.tausif@gmail.com>
2021-11-30 15:52:54 +0600

    worked on the list and event handler part 1

--- src/App.js
+++ src/App.js
@@ -1,88 +1,42 @@
-import React, { Component } from 'react';
-import './App.css';
-import First from './component/First/First';
-// import Counter from './component/Counter/Counter';
-import Example from './Example';
-
-
+import React, { Component } from "react";
+import "./App.css";
+import Books from './component/Books/Books';
 class App extends Component {
-/*
-  state = {
-    persons : [
-      {
-      name: 'SJ',
-      email: 'sjinnovation.com',
-      address: 'USA'
-      },
 
+  state = {
+    books: [
       {
-        name: 'ABCD',
-        email: 'aaa.com',
-        address: 'BD'
+        name: 'Javascript',
+        price: 20, 
+        id:1
+      },
+      {
+        name: 'PHP',
+        price: 30,
+        id:2
+      },
+      {
+        name: 'Laravel',
+        price: 40,
+        id:3
       }
     ]
   }
-  */
 
-  // clickHandler = () => {
-  //   alert('hello');
-  // }
-
-  state = {
-    name : ''
-  }
-
-  clickHandler = (event) => {
-    console.log(event.target);
-  }
-
-  inputHandler = (event) => {
-    // console.log(event.target.value);
-    // this.setState({
-    //   name: event.target.value
-    // });
+  deleteHandler = (id) => {
+    let newBooks = this.state.books.filter(book => book.id !== id);
     this.setState({
-      name: event.target.value
-      });
+      books: newBooks
+    })
   }
 
-
-  render(){
+  render() {
     return(
-      <div className="App">
-
-      {/* <First name={ this.state.persons[0].name } email={ this.state.persons[0].email } address={ this.state.persons[0].address }/>
-      <First name='Bahlul' email='ab@gmail.com' address='Bd'/>
-      <First name='Siddiquee' email='siddiquee@gmail.com' address='SYL'/> */}
-
-      {/* <Counter /> */}
-
-      {/* {
-        this.state.persons.map((people, index) => {
-          return <First 
-                        key={index}
-                        name={people.name}
-                        email={people.email} 
-                        address={people.address}
-                  />
-        })
-      } */}
-      
-      <input onChange={ this.inputHandler } type="text" value={ this.state.name } placeholder="Please enter your name" />
-
-      <button onClick={ this.clickHandler }>
-      {/* <button onClick={ (event) => console.log(event) }> */}
-        Click Me
-      </button>
-
-      { this.state.name ? <p> Hello MR. {this.state.name}</p> : '' }
-
-      
-      <Example name="Tausif"/>
-
-    </div>
-    );
-}
+      <div className='App'>
+        <Books deleteHandler ={() => this.deleteHandler.bind(this) } books ={ this.state.books }/>
+      </div>
+    )
+  }
 }
 
 export default App;
