a87fad14760e638545892caaac9aece24d0e7f26
Bahlul19 <bahlul.tausif@gmail.com>
2021-12-06 18:37:54 +0600

    Added the checkbox first functionality for the project

--- src/component/Forms/PostForm.js
+++ src/component/Forms/PostForm.js
@@ -4,7 +4,10 @@ const intialState = {
     name: '',
     email: '',
     password: '',
-    bio:''   
+    bio:'',
+    country:'',
+    gender:'',
+    skill:[]   
 }
 
 class PostForm extends Component {
@@ -85,6 +88,62 @@ class PostForm extends Component {
                     onChange={this.changeHandler}
             />
         </div><br/>
+
+        <div className='form-group'>
+
+            <select className='form-control' onChange={this.changeHandler} name='country' id='country'>
+                <option>Select your country</option>
+                <option value='Bangladesh'>Bangladesh</option>
+                <option value='India'>India</option>
+                <option value='Pakistan'>Pakistan</option>
+                <option value='USA'>USA</option>
+
+            </select>
+            <br/>
+
+        </div>
+
+        <div className='form-group'>
+            
+            <div className='form-check'>
+                <input 
+                onChange={this.changeHandler} 
+                type='radio' 
+                id='gender1' 
+                name='gender' 
+                value='Male'/>
+            <label htmlFor='gender'> Male</label>
+            </div>
+
+            <div className='form-check'>
+                <input 
+                onChange={this.changeHandler} 
+                type='radio' 
+                id='gender2' 
+                name='gender' 
+                value='Female'/>
+                <label htmlFor='gender'> Female</label>
+            </div>
+
+            <div className='form-check'>
+                <input 
+                onChange={this.changeHandler} 
+                type='radio' 
+                id='gender3' 
+                name='gender' 
+                value='Other'/>
+                <label htmlFor='gender'> Other</label>
+            </div>
+
+        </div>
+
+        <div className='form-group'>
+            <div className='form-check'>
+                <input className='form-check-input' id='js' value='JavaScript' type='checkbox'></input>
+            </div>
+        </div>
+
+
         <button className='btn btn-primary' type='submit'>Submit</button>
         </form>
        )
